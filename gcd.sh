5b84fbb5289d447567c8aea0e5825526c8ea1b09
rharada <a2432rh@aiit.ac.jp>
2025-06-07 03:47:31 +0000

    Added maxdigit to gcd.sh)

--- gcd.sh
+++ gcd.sh
@@ -5,8 +5,16 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-# 引数が自然数かチェック
+# 引数が自然数かチェック ※ここでは自然数に0を含むこととする
+
 if ! [[ "$1" =~ ^[0-9]+$ && "$2" =~ ^[0-9]+$ ]]; then
+  echo "Error: Both arguments must be natural numbers." >&2
+  exit 1
+fi
+
+# 引数のオーバーフロー対策。18桁までとする
+max_digits=18
+if [ ${#1} -gt $max_digits ] || [ ${#2} -gt $max_digits ]; then
   echo "Error: Both arguments must be natural numbers." >&2
   exit 1
 fi
